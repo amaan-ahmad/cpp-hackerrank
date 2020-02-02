ddceb0d4f601748cb836bb44f400e10830d80f9f
Amaan Ahmad <amaanahmad582001@gmail.com>
2020-02-02 13:09:32 +0530

    assign, pop_back, push_back, size()

--- vector.cpp
+++ vector.cpp
@@ -5,7 +5,16 @@ using namespace std;
 
 int main(){
     vector<int> obj1;
-    obj1.push_back(1);
-    cout << obj1[0];
+    //assigns vector 7 elements having value '12'.
+    obj1.assign(7, 12);
+
+    //removes last element
+    obj1.pop_back();
+
+    //inserts '5' at the end as element
+    obj1.push_back(5);
+
+    //prints last element, obj1.size get size of vector and -1 from it is the index number for last vector element.
+    cout << obj1[obj1.size()-1];
     return 0;
 }
