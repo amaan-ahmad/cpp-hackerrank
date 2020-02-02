1cf6130a1e2c532b04eca331f68f357d35491249
Amaan Ahmad <amaanahmad582001@gmail.com>
2020-02-02 16:02:58 +0530

    phase 2

--- vector.cpp
+++ vector.cpp
@@ -4,7 +4,10 @@
 using namespace std;
 
 int main(){
-    vector<int> obj1;
+
+    // # phase 1: init level #
+
+    vector<int> obj1, obj2;
     //assigns vector 7 elements having value '12'.
     obj1.assign(7, 12);
 
@@ -15,6 +18,20 @@ int main(){
     obj1.push_back(5);
 
     //prints last element, obj1.size get size of vector and -1 from it is the index number for last vector element.
-    cout << obj1[obj1.size()-1];
+    cout << obj1[obj1.size()-1] << endl;
+
+    // # phase 2: level++ #
+
+    //obj2 with elements : 8,8,8,1,3
+    obj2.assign(3, 8);
+    obj2.push_back(1);
+    obj2.push_back(3);
+
+    //print initial obj2
+    for(auto i=obj2.begin(); i != obj2.end(); ++i)
+        cout << *i << " ";
+
+
+
     return 0;
 }
